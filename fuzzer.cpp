972fb652566fe227a41683fe13045e61334221ad
johnagrillo@yahoo.com <johnagrillo>
2023-05-19 16:56:23 -0400

    dp not read strings

--- fuzzer.cpp
+++ fuzzer.cpp
@@ -47,9 +47,9 @@ extern "C" int LLVMFuzzerTestOneInput(uint8_t * data, size_t size)
   {
 
 
-    auto value = fdp.ConsumeIntegralInRange(0,500);
+    auto value = fdp.ConsumeIntegralInRange(0,1000);
 
-    if (value>400)
+    if (value>990)
     {
       if (img.pixels!=NULL)
 	img_free(&img);
@@ -65,11 +65,7 @@ extern "C" int LLVMFuzzerTestOneInput(uint8_t * data, size_t size)
     {
       auto vec = fdp.ConsumeBytes<uint8_t> (size);
       uint32_t * ptr = reinterpret_cast<uint32_t*>(vec.data());
-      if (ptr != NULL)
-      {
-       img_t img = img_from_abgr32(ptr, fdp.ConsumeIntegralInRange<unsigned>(0,2000), fdp.ConsumeIntegralInRange<unsigned>(0,2000));
-      if (img.pixels!=NULL) img_free(&img);
-      }
+
     }
     else if (val == 1)
     {
@@ -103,19 +99,10 @@ extern "C" int LLVMFuzzerTestOneInput(uint8_t * data, size_t size)
 
   else if (select == 1)   // Buffers
   {
-    auto val = fdp.ConsumeIntegralInRange(0, 1);
-    if (val == 0)
-    {
-      buffer_t * buf = buffer_create();  
-      std::string str = fdp.ConsumeRemainingBytesAsString();
-      buffer_write_char( buf, str.c_str(), str.size() );
-      if (buf != NULL) buffer_destroy(buf);
-    }
-    else if (val == 1)
-    {
-      buffer_t * buf = buffer_load(fdp.ConsumeRemainingBytesAsString().c_str());
-      if (buf != NULL) buffer_destroy(buf);
-    }
+    buffer_t * buf = buffer_create();  
+    std::string str = fdp.ConsumeRemainingBytesAsString();
+    buffer_write_char( buf, str.c_str(), str.size() );
+    if (buf != NULL) buffer_destroy(buf);
   }
   /*
    *  Testing Dir api 
@@ -123,13 +110,9 @@ extern "C" int LLVMFuzzerTestOneInput(uint8_t * data, size_t size)
 
   else if (select == 2)   // Dir
   {
-    auto val = fdp.ConsumeIntegralInRange(0, 0);
-    if (val == 0)
-    {
-      std::string str = fdp.ConsumeRemainingBytesAsString();
-      dir_t* dir = dir_open(str.c_str());
-      if (dir != NULL) dir_close(dir);
-    }
+    std::string str = fdp.ConsumeRemainingBytesAsString();
+    dir_t* dir = dir_open(str.c_str());
+    if (dir != NULL) dir_close(dir);
   }
   return 1;
 }
